fdcca15886cb4e84a7a23d2c8aa4a863a3410b74
Azizbek1205 <az1zbektop1gg@gmail.com>
2024-11-07 17:39:06 +0500

    home01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import oypon from "../assets/oypon.svg";
 
 function Home() {
   return (
     <div>
-      <a href=""></a>
+      <div className="home-box">
+        <div className="home">
+          <div className="home-top">
+            <div className="home-t-left">
+              <h4>Pro.Beyond.</h4>
+              <h1>
+                IPhone 14 <span>Pro</span>
+              </h1>
+              <p>Created to change everything for the better. For everyone</p>
+              <button className="t-white-btn">Shop Now</button>
+            </div>
+            <div className="home-t-right">
+              <img src={oypon} alt="" />
+            </div>
+          </div>
+          <div className="home-bottom">
+            <div className="home-b-left">
+              <div className="home-b-l-top"></div>
+              <div className="home-b-l-bottom">
+                <div className="home-b-l-b-left"></div>
+                <div className="home-b-l-b-right"></div>
+              </div>
+            </div>
+            <div className="home-b-right">
+              <div className="home-b-r-left">
+                <h1>
+                  Macbook <span>Air</span>
+                </h1>
+                <p>
+                  The new 15‑inch MacBook Air makes room for more of what you
+                  love with a spacious Liquid Retina display.
+                </p>
+                <button className="t-black-btn">Shop Now</button>
+              </div>
+              <div className="home-b-r-right"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
