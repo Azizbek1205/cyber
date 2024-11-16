302cd55a9edebae9d948f68bb2dc102e9aecf9a1
Azizbek1205 <az1zbektop1gg@gmail.com>
2024-11-16 15:01:47 +0500

    home03

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import oypon from "../assets/oypon.svg";
 import macbook from "../assets/macbook.svg";
 import PS5 from "../assets/PS5.svg";
+import headphones from "../assets/headphones.svg";
+import applevision from "../assets/applevision.svg";
 
 function Home() {
   return (
     <div>
-      <div className="home-box">
-        <div className="home">
+      <div className="home">
+        <div className="home-top-box">
           <div className="home-top">
             <div className="home-t-left">
               <h4>Pro.Beyond.</h4>
@@ -21,42 +23,59 @@ function Home() {
               <img src={oypon} alt="" />
             </div>
           </div>
-          <div className="home-bottom">
-            <div className="home-b-left">
-              <div className="home-b-l-top">
-                <div className="home-b-l-t-left">
-                  <img src={PS5} alt="" />
-                </div>
-                <div className="home-b-l-t-right">
-                  <h1>Playstation 5</h1>
-                  <p>
-                    Incredibly powerful CPUs, GPUs, and an SSD with integrated
-                    I/O will redefine your PlayStation experience.
-                  </p>
-                </div>
+        </div>
+        <div className="home-bottom">
+          <div className="home-b-left">
+            <div className="home-b-l-top">
+              <div className="home-b-l-t-left">
+                <img src={PS5} alt="" />
               </div>
-              <div className="home-b-l-bottom">
-                <div className="home-b-l-b-left">
-                  <div className="home-b-l-b-l-left"></div>
-                  <div className="home-b-l-b-l-right"></div>
-                </div>
-                <div className="home-b-l-b-right"></div>
+              <div className="home-b-l-t-right">
+                <h2>Playstation 5</h2>
+                <p>
+                  Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
+                  will redefine your PlayStation experience.
+                </p>
               </div>
             </div>
-            <div className="home-b-right">
-              <div className="home-b-r-left">
-                <h1>
-                  Macbook <span>Air</span>
-                </h1>
-                <p>
-                  The new 15‑inch MacBook Air makes room for more of what you
-                  love with a spacious Liquid Retina display.
-                </p>
-                <button className="t-black-btn">Shop Now</button>
+            <div className="home-b-l-bottom">
+              <div className="home-b-l-b-left">
+                <div className="home-b-l-b-l-left">
+                  <img src={headphones} alt="" />
+                </div>
+                <div className="home-b-l-b-l-right">
+                  <h2>
+                    Apple AirPods <span>Max</span>
+                  </h2>
+                  <p>Computational audio. Listen, it's powerful</p>
+                </div>
               </div>
-              <div className="home-b-r-right">
-                <img src={macbook} alt="" />
+              <div className="home-b-l-b-right">
+                <div className="home-b-l-b-r-left">
+                  <img src={applevision} alt="" />
+                </div>
+                <div className="home-b-l-b-r-right">
+                  <h2>
+                    Max Apple Vision <span>Pro</span>
+                  </h2>
+                  <p>An immersive way to experience entertainment</p>
+                </div>
               </div>
+            </div>
+          </div>
+          <div className="home-b-right">
+            <div className="home-b-r-left">
+              <h2>
+                Macbook <span>Air</span>
+              </h2>
+              <p>
+                The new 15‑inch MacBook Air makes room for more of what you love
+                with a spacious Liquid Retina display.
+              </p>
+              <button className="t-black-btn">Shop Now</button>
+            </div>
+            <div className="home-b-r-right">
+              <img src={macbook} alt="" />
             </div>
           </div>
         </div>
